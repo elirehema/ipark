37f15da33d2db8908600eeb97cfc20b5a9514b03
elirehema <rocksandystone@gmail.com>
2023-11-13 23:07:02 +0300

    Initial commit

---
+++ components/IndustrialMap.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="map-wrap" style="height: 100vh">
+ <client-only>
+   <l-map :zoom=13 :center="[-6.779932124032749, 38.71996284754632]">
+     <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+     <l-marker :lat-lng="[-6.779932124032749, 38.71996284754632]"></l-marker>
+   </l-map>
+ </client-only>
+</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <IndustrialMap/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
